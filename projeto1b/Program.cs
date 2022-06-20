ed97b834d87b9f10c0a7a678419de5a15f53fd44
Hisllaylla <hisllayllak@gmail.com>
2022-06-20 20:04:20 -0300

    integração do visual studio e upload ao git

--- projeto1b/Program.cs
+++ projeto1b/Program.cs
@@ -10,6 +10,27 @@ namespace projeto1b
     {
         static void Main(string[] args)
         {
+            int numero;
+            //cria locação de espaço na memoria para armazenar os dados do tipo pessoa
+            pessoa aluno;
+
+            //criação do objeto orientado
+            aluno = new pessoa();
+
+            //variação de uso
+            pessoa professor = new pessoa();
+
+            //uso do objeto em branco 
+            aluno.nome = "Igor";
+            professor.nome = "Prof. Welt";
+
+            //mostra o determinado uso
+            Console.WriteLine("Professor:" + professor.nome);
+            Console.WriteLine("Aluno:" + aluno.nome);
+            Console.ReadKey();
+
+
+
         }
     }
 }
---
+++ projeto1b/pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto1b
+{
+    // instanciando a classe transformando ela em um objeto
+    class pessoa
+    {
+        public string nome;
+        public string email;
+        public string endereço;
+    }
+}
